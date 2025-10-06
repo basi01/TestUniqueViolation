94bf2b7d2a7c775863bb30223ad757cd29d3c93e
Ilya Basin <ilya.basin.consultant@springernature.com>
2025-10-06 09:11:45 +0000

    x

--- build-scripts/900-run.bat
+++ build-scripts/900-run.bat
@@ -5,74 +5,35 @@ setlocal
 
 pushd %~dp0 || exit /b 1
 
+@echo starting the composition and detach...
+call docker compose -f 900-run-compose.yml up --wait || goto :err
 
-set "POSTGRES_DB=testuniqueviolation"
-set "POSTGRES_USER=testuniqueviolation"
-set "POSTGRES_PASSWORD=testuniqueviolation"
+@echo following the logs in a separate window...
+start cmd /c %~f0 goto :start_docker_compose_logs
 
-set "APP_TAG=testuniqueviolation-app"
-set "ADMIN_PASSWORD=n0t-A-s3cRet"
+@echo opening browser via socat proxy...
+start "" "http://localhost:4078"
 
-set sleepcommand=ping 127.0.0.1 -w 1 -n
+@echo Interactive proxy ready. You can launch http://localhost:4078 in your browser.
+@echo MxAdmin password: n0t-A-s3cRet
+@echo press any key to switch from app0 to app1
+@call docker attach testuniqueviolation-portproxy
 
-docker pull postgres >nul || exit /b 1
+:err
 
-start cmd /c %~f0 goto :start_postgres
+set ERRORLEVEL2=%ERRORLEVEL%
 
-call :get_container_host testuniqueviolation-postgres
-set DATABASE_HOST=%CONTAINER_HOST%
-
-echo DATABASE_HOST=%DATABASE_HOST%
-
-set "DATABASE_ENDPOINT=postgres://%POSTGRES_USER%:%POSTGRES_PASSWORD%@%DATABASE_HOST%/%POSTGRES_DB%"
-
-set CF_INSTANCE_INDEX=0
-start cmd /c %~f0 goto :start_app
-
-set CF_INSTANCE_INDEX=1
-start cmd /c %~f0 goto :start_app
+call docker compose -f 900-run-compose.yml down
 
 pause
+exit /b %ERRORLEVEL2%
 
-exit /b 0
-
-:start_postgres
-
-call docker run --rm -it --name testuniqueviolation-postgres -p 4079:5432 -m 2g ^
-  -e POSTGRES_DB ^
-  -e POSTGRES_USER ^
-  -e POSTGRES_PASSWORD ^
-  postgres
-
+@rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+:start_docker_compose_logs
+call docker compose -f 900-run-compose.yml logs -f
 pause
-
 goto :ennd
+@rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-:get_container_host
-
-@%sleepcommand% 2 >nul
-
-set CONTAINER_HOST=
-FOR /F "tokens=*" %%i IN ('call docker inspect -f "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}" %1') DO (
-  set CONTAINER_HOST=%%i
-)
-
-if "%CONTAINER_HOST%"=="" goto :get_container_host
-
-goto :ennd
-
-:start_app
-
-call docker run --rm -it ^
-  --name testuniqueviolation-app-%CF_INSTANCE_INDEX% ^
-  -p 408%CF_INSTANCE_INDEX%:8080 ^
-  -e CF_INSTANCE_INDEX ^
-  -e ADMIN_PASSWORD ^
-  -e DATABASE_ENDPOINT ^
-  "%APP_TAG%"
-
-pause
-
-goto :ennd
 
 :ennd
