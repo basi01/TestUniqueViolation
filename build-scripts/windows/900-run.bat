b20929f240de00eeb315f51e8f9bb746c8efbb66
Ilya Basin <ilya.basin.consultant@springernature.com>
2025-10-07 10:06:05 +0000

    y

--- build-scripts/windows/900-run.bat
+++ build-scripts/windows/900-run.bat
@@ -4,40 +4,45 @@ setlocal
 @rem run optional goto command:
 %*
 
-@set ERRORLEVEL2=1
-pushd %~dp0..\zzz-internal || goto :err
+pushd %~dp0..\zzz-internal || @goto :no_clean
 
-@set ERRORLEVEL2=0
-call :do_it || set ERRORLEVEL2=%ERRORLEVEL%
+call :do_it
 
-:err
-echo exiting with %ERRORLEVEL2%
-
+:clean
+set ERRORLEVEL2=%ERRORLEVEL%
+@rem final clean (this resets ERRORLEVEL)
 call docker compose -f 900-run-compose.yml down
+@goto :main_exit
 
-pause
+:no_clean
+set ERRORLEVEL2=%ERRORLEVEL%
+
+:main_exit
+echo exiting with %ERRORLEVEL2%
+%OPT_PAUSE% pause
 exit /b %ERRORLEVEL2%
 
 @rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 :do_it
 @echo starting the composition and detach...
-call docker compose -f 900-run-compose.yml up --wait || goto :ennd
+call docker compose -f 900-run-compose.yml up --wait || @goto :ennd
 @echo following the logs in a separate window...
-start cmd /c "%~f0" goto :start_docker_compose_logs
+start /MIN cmd /c "%~f0" goto :start_docker_compose_logs
 @echo opening browser via socat proxy...
 start "" "http://localhost:4078"
 @echo Interactive proxy ready. You can launch http://localhost:4078 in your browser.
+@echo Logs are opened in a separate window
 @echo MxAdmin password: n0t-A-s3cRet
 @echo press any key to switch from app0 to app1
-@call docker attach testuniqueviolation-portproxy || goto :ennd
-goto :ennd
+@call docker attach testuniqueviolation-portproxy || @goto :ennd
+@goto :ennd
 @rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 @rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 :start_docker_compose_logs
 call docker compose -f 900-run-compose.yml logs -f
 pause
-goto :ennd
+@goto :ennd
 @rem xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 :ennd
